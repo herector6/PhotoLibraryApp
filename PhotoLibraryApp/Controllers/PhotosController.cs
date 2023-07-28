45eeb43c56d54e120d3ecdb5c4c7e191b5f54253
Hector A Rodriguez <herector6@gmail.com>
2023-07-27 23:20:07 -0500

    a

--- PhotoLibraryApp/Controllers/PhotosController.cs
+++ PhotoLibraryApp/Controllers/PhotosController.cs
@@ -13,102 +13,69 @@ namespace PhotoLibraryApp.Controllers
     {
         private readonly PhotosDbContext _context;
         private readonly IWebHostEnvironment _hostingEnvironment;
-
         public PhotosController(PhotosDbContext context, IWebHostEnvironment hostingEnvironment)
         {
             _context = context;
             _hostingEnvironment = hostingEnvironment;
         }
-
         public IActionResult Index()
         {
             PhotoViewModel model = new PhotoViewModel(_context, _hostingEnvironment);
             return View(model);
         }
-
         [HttpPost]
-        public IActionResult Index(int photoId, string name, IFormFile imageFile, DateTime date)
+        public IActionResult CreateNewFile(int photoId, string name, IFormFile imageFile, DateTime date)
         {
             PhotoViewModel model = new PhotoViewModel(_context, _hostingEnvironment);
-
+                // Save the image file to the server
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    PhotoModel photo = new PhotoModel(photoId, name, imageFile, date);
+                    model.SavePhoto(photo);
+                    model.IsActionSuccess = true;
+                    model.ActionMessage = "The photo has been saved successfully!";
+                }
+            return View("Index", model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult SaveNewPicture(int photoId, string name, IFormFile imageFile, DateTime date) 
+        {
+            PhotoViewModel model = new PhotoViewModel(_context, _hostingEnvironment);
             if (ModelState.IsValid)
             {
                 // Save the image file to the server
                 if (imageFile != null && imageFile.Length > 0)
                 {
                     PhotoModel photo = new PhotoModel(photoId, name, imageFile, date);
-
                     model.SavePhoto(photo);
                     model.IsActionSuccess = true;
                     model.ActionMessage = "The photo has been saved successfully!";
                 }
             }
-
             return View(model);
         }
-
         public IActionResult Update(int id)
         {
             PhotoViewModel model = new PhotoViewModel(_context, id, _hostingEnvironment);
             return View(model);
         }
-
-<<<<<<< Updated upstream
-=======
-
-        [HttpPost]
-        public IActionResult Update(int photoID, string name, IFormFile imageFile, DateTime date)
-        {
-            PhotoViewModel model = new PhotoViewModel(_context, _hostingEnvironment);
-
-            if (ModelState.IsValid)
-            {
-                // Save the image file to the server (similar to the Index action)
-                if (imageFile != null && imageFile.Length > 0)
-                {
-                    /*string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
-                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        imageFile.CopyTo(fileStream);
-                    }
-                    
-                    // Update the image path
-                    string imagePath = filePath; // Or adjust according to your needs
-
-                    // Create the PhotoModel instance with imagePath instead of imageFile
-                    PhotoModel photo = new PhotoModel(photoID, name, imageFile, date);
-                */
-                    // Save the photo to the database
-                    //model.SavePhoto(photo);
-
-                    model.IsActionSuccess = true;
-                    model.ActionMessage = "The photo has been updated successfully!";
-                }
-            }
-
-            return View(model);
-        }
-
->>>>>>> Stashed changes
         public IActionResult Delete(int id)
         {
             PhotoViewModel model = new PhotoViewModel(_context, _hostingEnvironment);
-
             if (id > 0)
             {
                 model.RemovePhoto(id);
             }
-
             model.IsActionSuccess = true;
             model.ActionMessage = "Photo has been removed successfully.";
-
             return View("Index", model);
         }
     }
 }
+
 
 
 
